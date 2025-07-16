d5f749c1112f19a83bc31043e0e9867059df7fd7
jpe17 <joaopaesteves99@gmail.com>
2025-07-16 10:37:52 +0100

    Adjust

--- speak.py
+++ speak.py
@@ -1,5 +1,4 @@
-# In your Python script
 from infer_voice import speak_text
 
-# Generate and play audio
-speak_text("Hello, this is Elon Musk speaking through AI.")
+# Test your newly trained voice
+speak_text("Hello, my name is Bes")
